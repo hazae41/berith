831e42ccda091238dc88735489ba9e5de0865afd
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-10-04 23:49:15 +0200

    rename src to node

--- bench/deno/bench.ts
+++ bench/deno/bench.ts
@@ -1,7 +1,5 @@
 import * as ed from "https://deno.land/x/ed25519/mod.ts";
-import Ed25519, {
-  Ed25519Keypair
-} from "../../deno/mod.ts";
+import Ed25519, { Ed25519Keypair } from "../../deno/mod.ts";
 
 await Ed25519()
 
@@ -15,8 +13,8 @@ Deno.bench("ed25519_dalek", { group: "timing", baseline: true }, () => {
 
 Deno.bench("@noble/ed25519", { group: "timing" }, () => {
   const privateKey = ed.utils.randomPrivateKey();
-  const message = Uint8Array.from([0xab, 0xbc, 0xcd, 0xde]);
   const publicKey = ed.sync.getPublicKey(privateKey);
+  const message = Uint8Array.from([0xab, 0xbc, 0xcd, 0xde]);
   const signature = ed.sync.sign(message, privateKey);
   const isValid = ed.sync.verify(signature, message, publicKey);
 })
