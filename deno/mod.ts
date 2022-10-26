820292f565add06708e3ebeebf05e301d242fde5
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-10-26 20:34:02 +0200

    fix deno

--- deno/mod.ts
+++ deno/mod.ts
@@ -1,17 +1,3 @@
-export * from "../wasm/pkg/berith.js";
-export * as Berith from "../wasm/pkg/berith.js";
+export * from "./mods/mod.ts";
+export * as Berith from "./mods/mod.ts";
 
-import * as Base64 from "https://deno.land/std@0.158.0/encoding/base64.ts";
-
-import init, { InitOutput, initSync } from "../wasm/pkg/berith.js";
-import { wasm } from "../wasm/pkg/berith.wasm.js";
-
-let output: InitOutput | undefined = undefined
-
-export async function initSyncBundledOnce() {
-  return output ??= initSync(Base64.decode(wasm))
-}
-
-export async function initBundledOnce() {
-  return output ??= (init as any)(Base64.decode(wasm))
-}
